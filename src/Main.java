9a9ce90a40646af433542dfe89f7fea598b5488c
macklo001 <adres_macklo001@utp.edu.pl>
2021-03-18 19:01:26 +0100

    Lab1 01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
+    private String projectName = "Lab1 01";
     public static void main(String[] args) {
-        System.out.println("java version 1.8.0_181 JRE V1.7")
+        System.out.println("java version 1.8.0_181 JRE V1.7");
     }
 }
