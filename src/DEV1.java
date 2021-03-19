9065a03951143395c5a1804cd8338ed122b457ea
macklo001 <adres_macklo001@utp.edu.pl>
2021-03-19 17:38:52 +0100

    Lab1 v04

--- src/DEV1.java
+++ src/DEV1.java
@@ -1,4 +1,4 @@
-public class DEV1
+public class DEV1 {
         private String Author  = "Maciej Klonowski";
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    private String projectName = "Lab1 01";
+    private String Author  = "Maciej Klonowski";
+    private String Author = "Bartosz Babiarz";
     public static void main(String[] args) {
         System.out.println("java version 1.8.0_181 JRE V1.7");
     }
